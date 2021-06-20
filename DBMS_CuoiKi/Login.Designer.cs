806dd79e58f896fc834f7717999156e4d549a0cf
october-xxi <19110157@student.hcmute.edu.vn>
2021-06-20 09:32:24 +0700

    update

--- DBMS_CuoiKi/Login.Designer.cs
+++ DBMS_CuoiKi/Login.Designer.cs
@@ -133,6 +133,8 @@ namespace DBMS_CuoiKi
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
